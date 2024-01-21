d5140adbd76f7e053ef7c821a16cb637a5073f7f
Marcin Kozlowski <marcinkozlowski360@gmail.com>
2024-01-21 14:23:22 +0100

    toggleBackground fixed

--- css/js/script.js
+++ css/js/script.js
@@ -5,14 +5,16 @@
 
   const toggleBackground = () => {
     const body = document.querySelector(".body");
-    const changeBackgroundButton = document.querySelector(".body__button");
 
     body.classList.toggle("body__bright");
   };
 
   const init = () => {
+    const changeBackgroundButton = document.querySelector(".body__button");
     changeBackgroundButton.addEventListener("click", toggleBackground);
+
+    welcome();
   };
 
-  welcome();
+  init();
 }
